7ac089fbe2f534a5ce650d73b1636d058166c959
keeponZhang <462789909@qq.com>
2019-04-13 18:34:36 +0800

    no message

--- app/src/main/java/com/darren/eventdispatch/TestLinearLayout.java
+++ app/src/main/java/com/darren/eventdispatch/TestLinearLayout.java
@@ -53,7 +53,7 @@ public class TestLinearLayout extends LinearLayout {
     dispatchTransformedTouchEvent会返回true，if里面的代码会走，mFirstTouchTarget就不为null，这时代码走下去，handled为true，
     接着会回溯上去（倒数第二层的mFirstTouchTarget为倒数第一层的view，倒数第三层的mFirstTouchTarget为倒数第二层的view（倒数第三层handled也为true））
     结论2：dispatchTouchEvent 和 onTouchEvent 一旦return true,事件就停止传递了（到达终点）（没有谁能再收到这个事件）。
-     看下图中只要return true事件就没再继续传下去了，对于return true我们经常说事件被消费了，消费了的意思就是事件走到这里就是终点，不会往下传，没有谁能再收到这个事件了（此时down事件不会传到父控件的onTouchEvent中）
+     看下图中只要return true事件就没再继续传下去了，对于return true我们经常说事件被消费了，消费了的意思就是事件走到这里就是终点，不会往下传，没有谁能再收到这个事件了（此时down事件不会传到父容器的onTouchEvent中）
      （这里有一个注意点，同一个view,onTouchEvent返回true,如果dispatchTouchEvent方法返回调用的是super.dispatchTouchEvent,这dispatchTouchEvent也会返回true,其他情况则不一定）
     */
     /*3.加入的最后的child的dispatchTouchEvent返回true,一般来说，父控件就不走onTouchEvent?有没办法实现走父控件的onTouchEvent呢?
@@ -63,8 +63,9 @@ public class TestLinearLayout extends LinearLayout {
     */
     /*4.父控件的onInterceptTouchEvent返回true，表示父控件拦截了事件，子类就再也收不到事件，有没办法在该情况下，子view也能受到事件呢？
       有的，但是有一个前提，父控件不能拦截actionDown事件，因为父控件拦截actionDown后，事件走不到子view中，你在actiondown事件前设置的不允许父控件拦截标志位，会在actiondown中被重置
-      所以一般做法是在父控件onInterceptTouchEvent方法中，如果actionDown事件返回false，其他事件返回true。然后在子view的dispatchTouchEvent的actionDown调用 getParent().requestDisallowInterceptTouchEvent(true)，true表示不允许父控件拦截。(注意，如果子view没有调用getParent().requestDisallowInterceptTouchEvent(true)方法，子view不会受到move 和up事件，会收到cancel事件)
-      (这里还有一个注意点，因为down事件返回false，此时也不会调用父控件的onTouchEvent方法。）
+      所以一般做法是在父控件onInterceptTouchEvent方法中，如果actionDown事件返回false，其他事件返回true。然后在子view的dispatchTouchEvent的actionDown调用 getParent().requestDisallowInterceptTouchEvent(true)，true表示不允许父控件拦截。(注意，如果子view消费了down事件，并且子view没有调用getParent().requestDisallowInterceptTouchEvent(true)方法，子view不会受到move 和up事件，会收到cancel事件)
+      (这里还有一个注意点，因为父控件onInterceptTouchEventdown事件返回false，onInterceptTouchEvent其他事件返回true，子类消费了down事件，此时也不会调用父控件的onTouchEvent方法（因为down事件没拦截，mFirstTouchTarget不等于null，down事件是不会调用
+      dispatchTransformedTouchEvent，move和up事件，onInterceptTouchEvent其他事件返回true，不会走  if (!canceled && !intercepted)，此时因为mFirstTouchTarget不等于null，alreadyDispatchedToNewTouchTarget为false，intercepted为true，会给子view分发一个cancel事件）。）
      */
     /*
      5.view事件分发给自己的onTouchEvent 处理呢，那只能return super.dispatchTouchEvent,View类的dispatchTouchEvent（）方法默认实现就是能帮你调用View自己的onTouchEvent方法的。然后ViewGroup怎样通过dispatchTouchEvent方法能把事件分发到自己的onTouchEvent处理呢，return true和false 都不行，那么只能通过Interceptor把事件拦截下来给自己的onTouchEvent，所以ViewGroup dispatchTouchEvent方法的super默认实现就是去调用onInterceptTouchEvent
@@ -80,13 +81,15 @@ public class TestLinearLayout extends LinearLayout {
      */
     /*
      8. 除了down事件的其他事件，只有当前viewGroup的mTouchTarget不为null，才会调用onInterceptTouchEvent方法（即是如果onInterceptTouchEvent的down事件返回true，这回导致事件传到当前viewgroup的onTouchEvent方法，mTouchTarget为null，接着的其他事件不会走onInterceptTouchEvent方法）
+      *(即是才会调用onInterceptTouchEvent方法走的两个条件，down事件或者mFirstTouchTarget != null，即actionMasked == MotionEvent.ACTION_DOWN || mFirstTouchTarget != null）
+
      */
     /**
      9.clearTouchTargets先释放当前的target到复用池，如果当前target的nextTarget不为null，则把nextTarget也释放到复用池
      /*
      10. onInterceptTouchEvent down返回false,move和up拦截，如果子view消费了事件，
      mTouchTarget不为null，dispatchTransformedTouchEvent(ev, cancelChild,
-     target.child, target.pointerIdBits)) 会给消费该事件的子view传递一个action_cancel事件，之后的事件就不会再传给该子view。
+     target.child, target.pointerIdBits)) 会给消费该事件的子view传递一个action_cancel事件，之后的事件就不会再传给该子view（该条件下该viewGroup不会收到onTouchEvent）。
      */
     /*
      11.如果View不消耗除ACTION_DOWN以外的其它事件，那么这个点击事件不会消失，此时父元素的onTouchEvent()并不会调用，并且当前View可以持续收到后续的事件，最终这些消失的点击事件会传递给Activity处理。(父容器不拦截的话,不会发出cancel事件，mTouchTarget不会置为null，接下来的触摸事件还会传到子view)
